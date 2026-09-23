35ea9fda3014d702a376d940dd91f5f70d7836ea
AntonioRmz <125052853@upq.edu.mx>
2026-09-22 19:53:29 -0600

    Se hizo correccion de codigo y la terminacion del codigo, tambien se implemento un menu para que funcione de manera independiente

--- practica2/Caja.js
+++ practica2/Caja.js
@@ -1,22 +1,127 @@
-function
-calcularSubtotal(pedidos){
+const readline = require("readline/promises");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let pedidos = [
+    {
+        producto: "Pizza",
+        cantidad: 2,
+        total: 240
+    },
+    {
+        producto: "Hamburguesa",
+        cantidad: 1,
+        total: 80
+    },
+    {
+        producto: "Refresco",
+        cantidad: 2,
+        total: 60
+    }
+];
 
-    let subtotal = pedidos.reduce(function(total, pedido){
+function listarPedidos() {
+    console.log("");
+    console.log(" PEDIDOS ");
+    pedidos.forEach(function(pedido) {
+        console.log(
+            `${pedido.producto} x${pedido.cantidad} = $${pedido.total}`
+        );
+    });
+}
+
+function calcularSubtotal() {
+    let subtotal = pedidos.reduce(function(total, pedido) {
         return total + pedido.total;
-    },0);
+    }, 0);
     return subtotal;
 }
 
-function calcularIVA(subtotal){
-
+function calcularIVA() {
+    let subtotal = calcularSubtotal();
     let iva = subtotal * 0.16;
     return iva;
 }
 
-function mostrarResumen(pedidos){
-
-    if (pedidos.length == 0){
-        console.log("\nNo hay pedidos");
-        return;
-    }
+function calcularTotal() {
+    let subtotal = calcularSubtotal();
+    let iva = subtotal * 0.16;
+    let total = subtotal + iva;
+    return total;
 }
+
+function mostrarResumen() {
+    console.log("");
+    console.log("");
+    console.log(" RESUMENe DE CAJA");
+    console.log("");
+    pedidos.forEach(function(pedido) {
+        let {
+            producto,
+            cantidad,
+            total
+        } = pedido;
+        console.log(
+            `${producto} x${cantidad} = $${total}`
+        );
+    });
+
+    let subtotal = calcularSubtotal();
+    let iva = calcularIVA();
+    let total = calcularTotal();
+
+    console.log("");
+    console.log(`Subtotal: $${subtotal.toFixed(2)}`);
+    console.log(`IVA: $${iva.toFixed(2)}`);
+    console.log(`Total: $${total.toFixed(2)}`);
+    console.log("");
+}
+async function menu() {
+    let opcion;
+    do {
+        console.log(`
+            CAJA
+1. Mostrar pedidos
+2. Calcular subtotal
+3. Calcular IVA
+4. Calcular total
+5. Mostrar resumen
+6. Salir
+`);
+
+        opcion = await rl.question("Elige una opción: ");
+        if (opcion == "1") {
+            listarPedidos();
+        }
+        else if (opcion == "2") {
+            console.log(
+                "Subtotal: $",
+                calcularSubtotal().toFixed(2)
+            );
+        }
+        else if (opcion == "3") {
+            console.log(
+                "IVA: $",
+                calcularIVA().toFixed(2)
+            );
+        }
+        else if (opcion == "4") {
+            console.log(
+                "Total: $",
+                calcularTotal().toFixed(2)
+            );
+        }
+        else if (opcion == "5") {
+            mostrarResumen();
+        }
+        else if (opcion == "6") {
+            console.log("Saliendo del modulo Caja.");
+        }
+        else {
+            console.log("Opción no válida.");
+        }
+    } while (opcion != "6");
+    rl.close();
+}
+menu();
